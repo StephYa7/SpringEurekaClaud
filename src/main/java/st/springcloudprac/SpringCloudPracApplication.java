de63c825ec5ae5ffdb5136d09ccdd26f3b764154
StephYa7 <1000p@mail.ru>
2024-02-29 20:51:42 +0700

    added screenshot

--- src/main/java/st/springcloudprac/SpringCloudPracApplication.java
+++ src/main/java/st/springcloudprac/SpringCloudPracApplication.java
@@ -2,8 +2,10 @@ package st.springcloudprac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringCloudPracApplication {
 
     public static void main(String[] args) {
