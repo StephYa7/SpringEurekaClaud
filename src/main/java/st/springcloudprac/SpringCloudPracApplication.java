a9133f3ce4734b58cb2461fdeacc147726a3bfff
StephYa7 <1000p@mail.ru>
2024-02-29 20:44:36 +0700

    Initial commit

---
+++ src/main/java/st/springcloudprac/SpringCloudPracApplication.java
@@ -0,0 +1,13 @@
+package st.springcloudprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/st/springcloudprac/SpringCloudPracApplicationTests.java
@@ -0,0 +1,13 @@
+package st.springcloudprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
